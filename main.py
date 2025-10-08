3844d2269dc7455736bc9eeb31310678728bd65b
sonika-25 <64954778+sonika-25@users.noreply.github.com>
2025-10-08 23:54:47 +1100

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ language_options = ["English"]
 
 # Main app layout
 def main():
-    st.subheader("LinkedIn Post Generator: Codebasics")
+    st.subheader("LinkedIn Post Generator! With Author John Green")
 
     # Create three columns for the dropdowns
     col1, col2, col3 = st.columns(3)
